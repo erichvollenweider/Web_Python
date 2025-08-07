0d7734b491bf7ed2e6787782d61e56316af5913f
erichvollenweider <vollenweidererich@yahoo.com.ar>
2025-08-07 20:15:56 -0300

    Construccion del frontend en remoto

--- remote_build.sh
+++ remote_build.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-source env/bin/activate
+cd Web_Python
+python -m venv .venv
+source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-#API_URL=erichvollenweider-web.up.railway.app
-reflex export --frontend-only
+reflex export --frontend-only --api-url $API_URL
 unzip frontend.zip -d public
 rm -f frontend.zip
 # deactivate  # opcional: puede fallar, no es grave
